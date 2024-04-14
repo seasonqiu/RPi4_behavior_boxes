7e914db8fb5032f9f11ecb288ed0b4c565736766
season <season.qiu@yahoo.com>
2024-04-14 09:35:38 -0400

    start_recording revamp

--- start_recording.py
+++ start_recording.py
@@ -11,25 +11,25 @@ def signal_handler(signum, frame):
     camera.stop_recording()
     camera.stop_preview()
     print('Recording Stopped')
-    output.close()
+#    output.close()
     print('Closing Output File')
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
 base_path = '~/test'
 camId = str(0)
-# VIDEO_FILE_NAME = base_path + "_cam" + camId + "_output_" + str(dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".h264"
+video_file_name = base_path + "_cam" + camId + "_output_" + \
+                  str(dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".h264"
+time_stamp_file_name = base_path + "_cam" + camId + "_timestamp_" + \
+                       str(dt.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".txt"
 
 camera = Picamera2()
 video_config = camera.create_video_configuration()
 camera.configure(video_config)
 encoder = H264Encoder(bitrate=10000000)
-output = "/home/pi/test.h264"
-camera.start_preview(Preview.DRM)
-camera.start()
-time.sleep(2)
-camera.start_recording(encoder, output)
-signal.signal(signal.SIGINT, signal_handler)
-signal.pause()
-# time.sleep(10)
-# picam2.stop_recording()
+
+camera.start_preview(Preview.QT)
+camera.start_recording(encoder, video_file_name, pts=time_stamp_file_name)
+time.sleep(10)
+camera.stop_recording()
+camera.stop_preview()
